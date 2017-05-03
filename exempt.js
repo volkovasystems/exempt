b204eb9772aacab1dd627a3334ac3ec958fcfae6
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 18:54:24 +0800

    Improved code.

--- exempt.js
+++ exempt.js
@@ -34,6 +34,9 @@
 			"file": "excursio.js",
 			"module": "excursio",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/excursio.git",
 			"global": true
