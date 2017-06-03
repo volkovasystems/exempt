bd5a857391723a6f8aecbc6887077c40495e11a8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-03 15:57:10 +0800

    Updated test.

--- exempt-test.js
+++ exempt-test.js
@@ -7,4 +7,6 @@ assert.deepEqual( exempt( list, 3 ) === list, true, "should be true" );
 let residue = [ ];
 assert.deepEqual( exempt( list, 5, residue ), [ 1, 2, 4, 6, 7, 8, 9 ] );
 
+assert.deepEqual( exempt( [ 1, 4, 3, 2 ], 2 ), [ 1, 4, 3 ], "should be equal" );
+
 console.log( "ok" );
